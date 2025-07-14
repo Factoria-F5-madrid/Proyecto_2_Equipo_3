35582c41cee87c790ee40694b0873a0adaf0ab72
Ignacio Castillo Franco <github_portfolio@outlook.es>
2025-07-14 09:52:24 +0200

    chore/ pip install psycopg2-binary and changed DB settings

--- Food5/Food5/settings.py
+++ Food5/Food5/settings.py
@@ -15,7 +15,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent #points to the root directory of your Django project (the one containing manage.py).__file__ is the path to the current file (settings.py).parent.parent goes up two directories:
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'Food5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food5_db',
+        'USER': 'dbuser',
+        'PASSWORD': 'dbpass',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
