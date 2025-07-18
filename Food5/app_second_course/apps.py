8d0398114011f2ae31dfe04534f2eb879681b774
Arggendion <teoramosruano@gmail.com>
2025-07-18 13:15:03 +0200

    feature/app_second_course

---
+++ Food5/app_second_course/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppSecondCourseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_second_course'
---
+++ Food5/app_second_course/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from Food5.Food5_app.models import Dish
+
+# Create your models here.
+class SecondCourse(Dish):
+    
+    pass
