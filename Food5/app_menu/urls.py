fd1fad4aae438e66ebf1c309684ab6a100254414
Ignacio Castillo Franco <github_portfolio@outlook.es>
2025-07-23 15:19:38 +0200

    tests: menu tests OK

--- Food5/app_menu/urls.py
+++ Food5/app_menu/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import listar_menus, crear_menu, detalle_menu
 
 urlpatterns = [
-    path('', listar_menus),
-    path('create/', crear_menu),
-    path('<int:pk>/', detalle_menu),
+    path('', listar_menus, name='listar_menus'),
+    path('create/', crear_menu, name='crear_menus'),
+    path('<int:pk>/', detalle_menu, name='detallar_menus'),
 ]
