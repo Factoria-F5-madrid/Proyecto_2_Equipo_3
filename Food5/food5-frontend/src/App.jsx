0beaeb1830c54e2c240b88ec4f843369f6e88113
Andres <andres.lazaro.nu@gmail.com>
2025-07-24 14:41:01 +0200

    Frontend setup: estructura con Vite y React, integración de Bootstrap, routing con React Router, Navbar responsive, página Home y Login pagina Login

---
+++ Food5/food5-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Navbar />
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+    
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Food5/food5-frontend/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+// src/components/Navbar.jsx
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      <Link to="/" className="navbar-brand">Food5</Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-link">Register</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/menu" className="nav-link">Menú</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/orders" className="nav-link">Pedidos</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
